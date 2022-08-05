326efa031ec431e967ef4a548fefb78d80860197
Federico <33469732+FedeMariotta@users.noreply.github.com>
2022-08-05 12:16:23 -0300

    Add files via upload

---
+++ leerTextoCamara.py
@@ -0,0 +1,29 @@
+#Abre la camara, busca texto en cada frame y lo muestra en consola
+
+import cv2
+import pytesseract 
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    text = pytesseract.image_to_string(frame) #Pasa la imagen a texto
+    print(text)
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
